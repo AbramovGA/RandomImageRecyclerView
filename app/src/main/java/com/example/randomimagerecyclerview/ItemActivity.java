c87197a477bb66dd892c97ffb8a81a35364eb37b
gera <german_abramov@epam.com>
2019-05-20 19:46:22 +0300

    fix RxJava

--- app/src/main/java/com/example/randomimagerecyclerview/ItemActivity.java
+++ app/src/main/java/com/example/randomimagerecyclerview/ItemActivity.java
@@ -8,15 +8,19 @@ import android.widget.TextView;
 
 import com.example.randomimagerecyclerview.model.Post;
 import com.example.randomimagerecyclerview.network.JSONPlaceholderService;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-import io.reactivex.Observable;
+import io.reactivex.Completable;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.CompletableSubject;
 import retrofit2.Response;
 
 import static android.view.View.INVISIBLE;
@@ -32,50 +36,66 @@ public class ItemActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
 
-        loadImageFromSomwhere();
-        loadTextFromSomwhere().observeOn(AndroidSchedulers.mainThread())
-                .doOnComplete(() -> {
+        Disposable subscribe = Completable
+                .mergeArray(loadImageFromSomwhere(),
+                        loadTextFromSomwhere())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(() -> {
                     findViewById(R.id.item_screen).setVisibility(VISIBLE);
                     findViewById(R.id.progress).setVisibility(INVISIBLE);
                 });
     }
 
-    private void loadImageFromSomwhere() {
+    private Completable loadImageFromSomwhere() {
+        CompletableSubject subject = CompletableSubject.create();
         String imageUrl = getImgFromIntent().orElse(IMG_NOT_FOUND);
         ImageView imageView = findViewById(R.id.item_image);
-        Picasso.get().load(imageUrl).into(imageView);
+        Picasso.get().load(imageUrl).into(imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                subject.onComplete();
+            }
+
+            @Override
+            public void onError(Exception e) {
+                subject.onError(e);
+            }
+        });
+
+        return subject;
     }
 
-    private Observable loadTextFromSomwhere() {
+    private Completable loadTextFromSomwhere() {
         TextView textView = findViewById(R.id.item_description);
-        return Single.fromCallable(this::loadText)
+        return loadText().subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .doAfterSuccess((text) -> textView.setText(text))
-                .toObservable();
+                .doOnSuccess(textView::setText)
+                .ignoreElement();
     }
 
 
-    private String loadText() {
+    private Single<String> loadText() {
+        return Single.fromCallable(() -> {
+            JSONPlaceholderService service = JSONPlaceholderService.init();
 
-        JSONPlaceholderService service = JSONPlaceholderService.init();
+            Response<List<Post>> response = null;
+            try {
+                response = service.listPosts().execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        Response<List<Post>> response = null;
-        try {
-            response = service.listPosts().execute();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            String text = response.body().get(0).getBody();
 
-        String text = response.body().get(0).getBody();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            return text;
 
-        return text;
-
+        });
     }
 
     private Optional<String> getImgFromIntent() {
